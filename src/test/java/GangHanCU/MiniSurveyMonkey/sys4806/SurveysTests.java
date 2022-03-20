34af3a2ec72259bf5063d286c8cc92302ec862ca
GangHan-CU <masonhan@cmail.carleton.ca>
2022-03-20 19:07:28 +0800

    add html

---
+++ src/test/java/GangHanCU/MiniSurveyMonkey/sys4806/SurveysTests.java
@@ -0,0 +1,14 @@
+package GangHanCU.MiniSurveyMonkey.sys4806;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class SurveysTests {
+
+}
